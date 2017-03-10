af63db075fab0c7418004aa6ad6e3b6dbf6132d3
kalyskin <kalysrysmendeev@gmail.com>
2017-03-10 18:14:57 +0300

    added keyboard control2

--- app.jsx
+++ app.jsx
@@ -162,6 +162,7 @@ var GridTamplate = [
         y: 3,
     },
 ];
+var TMP = {};
 
 var Box = React.createClass({
     getInitialState: function () {
@@ -372,8 +373,6 @@ var App = React.createClass({
                     }
                 }
                 break;
-
-
         }
 
         var bool = false;
@@ -402,14 +401,19 @@ var App = React.createClass({
         });
         self.setState({});
         setTimeout(function () {
+            console.log(this.diff());
             tmp_box.active = 1;
             self.setState({});
+            TMP = Object.assign({}, this.state.grid);
         },200);
         if(self.state.score > self.state.score_max){
             self.state.score_max = self.state.score;
             localStorage.setItem('score_2048',self.state.score_max);
         }
 
+    },
+    diff: function () {
+        return deepDiffMapper.map(TMP,this.state.grid);
     },
     isEmpty: function (e) {
         var re = null;
@@ -458,3 +462,77 @@ ReactDOM.render(
     document.getElementById("content")
 );
 
+var deepDiffMapper = function() {
+    return {
+        VALUE_CREATED: 'created',
+        VALUE_UPDATED: 'updated',
+        VALUE_DELETED: 'deleted',
+        VALUE_UNCHANGED: 'unchanged',
+        map: function(obj1, obj2) {
+            if (this.isFunction(obj1) || this.isFunction(obj2)) {
+                throw 'Invalid argument. Function given, object expected.';
+            }
+            if (this.isValue(obj1) || this.isValue(obj2)) {
+                return {
+                    type: this.compareValues(obj1, obj2),
+                    data: (obj1 === undefined) ? obj2 : obj1
+                };
+            }
+
+            var diff = {};
+            for (var key in obj1) {
+                if (this.isFunction(obj1[key])) {
+                    continue;
+                }
+
+                var value2 = undefined;
+                if ('undefined' != typeof(obj2[key])) {
+                    value2 = obj2[key];
+                }
+
+                diff[key] = this.map(obj1[key], value2);
+            }
+            for (var key in obj2) {
+                if (this.isFunction(obj2[key]) || ('undefined' != typeof(diff[key]))) {
+                    continue;
+                }
+
+                diff[key] = this.map(undefined, obj2[key]);
+            }
+
+            return diff;
+
+        },
+        compareValues: function(value1, value2) {
+            if (value1 === value2) {
+                return this.VALUE_UNCHANGED;
+            }
+            if (this.isDate(value1) && this.isDate(value2) && value1.getTime() === value2.getTime()) {
+                return this.VALUE_UNCHANGED;
+            }
+            if ('undefined' == typeof(value1)) {
+                return this.VALUE_CREATED;
+            }
+            if ('undefined' == typeof(value2)) {
+                return this.VALUE_DELETED;
+            }
+
+            return this.VALUE_UPDATED;
+        },
+        isFunction: function(obj) {
+            return {}.toString.apply(obj) === '[object Function]';
+        },
+        isArray: function(obj) {
+            return {}.toString.apply(obj) === '[object Array]';
+        },
+        isDate: function(obj) {
+            return {}.toString.apply(obj) === '[object Date]';
+        },
+        isObject: function(obj) {
+            return {}.toString.apply(obj) === '[object Object]';
+        },
+        isValue: function(obj) {
+            return !this.isObject(obj) && !this.isArray(obj);
+        }
+    }
+}();
